d2ba0c11896fa4f27ac4a8991d6e13d432c193ed
Shivam Gupta <shivamgupta86923@gmail.com>
2025-10-11 17:47:13 +0530

    Update Categories.jsx

--- my-react-app/src/components/home/Categories.jsx
+++ my-react-app/src/components/home/Categories.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Button, Table, TableBody, TableCell, TableHead, TableRow, styled } from '@mui/material'
+import { Button, Table, TableBody, TableCell, TableHead, TableRow, styled, Box, useMediaQuery, useTheme, Stack } from '@mui/material'
 import { categories } from '../../constants/data'
 import {Link, useSearchParams} from 'react-router-dom';
 import { DataContext } from '../../context/DataProvider';
@@ -8,53 +8,97 @@ const StyledTable = styled(Table)`
     border: 1px solid rgba(224, 224, 224, 1);
 `;
 
-const StyledButton = styled(Button)`
+const StyledButton = styled(Button)(({ theme }) => `
     margin: 20px;
     width: 80%;
     background: #6495ED;
     color: #fff;
-`;
+    
+    ${theme.breakpoints.down('sm')} {
+        margin: 16px;
+        width: 100%;
+        font-size: 0.875rem;
+    }
+`);
+
 const StyledLink = styled(Link)`
-    text-decoration:none;
+    text-decoration: none;
     color: inherit;
 `;
+
+const CategoryContainer = styled(Box)(({ theme }) => `
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    
+    ${theme.breakpoints.down('sm')} {
+        padding: 8px;
+        width: 100vw;
+        margin-left: calc(-50vw + 50%);
+    }
+`);
+
+const ResponsiveTableCell = styled(TableCell)(({ theme }) => `
+    text-align: center;
+    
+    ${theme.breakpoints.down('sm')} {
+        padding: 12px 8px;
+        font-size: 0.875rem;
+    }
+`);
+
 const Categories = () => {
-    const {account} = useContext(DataContext);
+    const { account } = useContext(DataContext);
     const [searchParams] = useSearchParams();
     const category = searchParams.get('category');
-  return (
-    <div>
-    {account?.user_type === "Teacher" && (
-                <StyledLink to={`/create?category=${category || ''}`}>
-                    <StyledButton variant='contained'>Create Blog</StyledButton>
-                </StyledLink>
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+    return (
+        <CategoryContainer>
+            {account?.user_type === "Teacher" && (
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                    <StyledLink to={`/create?category=${category || ''}`}>
+                        <StyledButton variant='contained'>
+                            {isMobile ? 'Create' : 'Create Blog'}
+                        </StyledButton>
+                    </StyledLink>
+                </Box>
             )}
-      <StyledTable>
-        <TableHead>
-            <TableRow>
-                <TableCell>
-                <StyledLink to='/'>
-                    All Categories
-                </StyledLink>
-                </TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-            {
-                categories.map(category=>(
-                    <TableRow key={category.id}>
-                        <TableCell>
-                        <StyledLink to={`/?category=${category.name}`}>
-                            {category.name}
-                            </StyledLink>
-                        </TableCell>
-                     </TableRow>
-                ))
-            }
-        </TableBody>
-      </StyledTable>
-    </div>
-  )
+            
+            <Box sx={{ overflowX: 'auto' }}>
+                <StyledTable size={isMobile ? 'small' : 'medium'}>
+                    <TableHead>
+                        <TableRow sx={{ backgroundColor: 'f5f5f5' }}>
+                            <ResponsiveTableCell sx={{ fontWeight: 'bold' }}>
+                                <StyledLink to='/'>
+                                    All Categories
+                                </StyledLink>
+                            </ResponsiveTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {categories.map(cat => (
+                            <TableRow key={cat.id} sx={{
+                                '&:hover': {
+                                    backgroundColor: 'rgba(100, 149, 237, 0.05)',
+                                },
+                                cursor: 'pointer'
+                            }}>
+                                <ResponsiveTableCell>
+                                    <StyledLink to={`/?category=${cat.name}`}>
+                                        {cat.name}
+                                    </StyledLink>
+                                </ResponsiveTableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </StyledTable>
+            </Box>
+        </CategoryContainer>
+    )
 }
 
 export default Categories
