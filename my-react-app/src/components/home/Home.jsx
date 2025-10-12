9cb2492b72dce7f94e6939575eadb82844a9bca2
Shivam Gupta <shivamgupta86923@gmail.com>
2025-10-12 13:01:19 +0530

    Update Home.jsx

--- my-react-app/src/components/home/Home.jsx
+++ my-react-app/src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Banner from '../banner/Banner';
 import Categories from './Categories';
-import { Grid, Container, Box, useMediaQuery, useTheme } from '@mui/material'; 
+import { Grid, Box, useMediaQuery, useTheme } from '@mui/material'; 
 import Posts from './Posts';
 
 const Home = () => {
@@ -11,40 +11,36 @@ const Home = () => {
   return (
     <>
       <Banner />
-      <Container maxWidth="lg" sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2} sx={{ marginTop: 1 }}>
-          {/* Categories Sidebar */}
-          <Grid 
-            item 
-            lg={2} 
-            md={3}
-            sm={12}
-            xs={12}
-            sx={{
-              order: { xs: 2, sm: 2, md: 1, lg: 1 },
-              borderRight: { lg: '1px solid #e0e0e0', md: 'none', sm: 'none', xs: 'none' }
-            }}
-          >
+      
+      {isMobile ? (
+        // Mobile Layout - Categories Full Width, Posts below
+        <Box sx={{ width: '100%', padding: '0 0' }}>
+          <Grid item xs={12} sx={{ width: '100%' }}>
             <Categories />
           </Grid>
-
-          {/* Posts Grid */}
           <Grid 
             container 
-            item 
-            lg={10} 
-            md={9}
-            sm={12}
-            xs={12}
-            spacing={{ lg: 2, md: 2, sm: 2, xs: 1 }}
+            spacing={1}
             sx={{
-              order: { xs: 1, sm: 1, md: 2, lg: 2 },
+              width: '100%',
+              padding: '0 8px',
+              margin: 0
             }}
           >
             <Posts />
           </Grid>
+        </Box>
+      ) : (
+        // Desktop/Tablet Layout - Original 2 column
+        <Grid container sx={{ width: '100%' }}>
+          <Grid item lg={2} sm={2} xs={12}>
+            <Categories />
+          </Grid>
+          <Grid container item lg={10} sm={10} xs={12}>
+            <Posts />
+          </Grid>
         </Grid>
-      </Container>
+      )}
     </>
   )
 }
