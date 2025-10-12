94c23bd855c272e508a035baca7a5ef41a4c897c
Shivam Gupta <shivamgupta86923@gmail.com>
2025-10-12 22:34:56 +0530

    Update upload.js

--- Backend/utils/upload.js
+++ Backend/utils/upload.js
@@ -27,17 +27,31 @@
 // export default multer({ storage });
 
 
-import multer from 'multer';
+// import multer from 'multer';
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, Date.now() + '-' + file.originalname)
-    }
-  })
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, './uploads')
+//     },
+//     filename: function (req, file, cb) {
+//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//       cb(null, Date.now() + '-' + file.originalname)
+//     }
+//   })
   
-  const upload = multer({storage })
-  export default upload;
+//   const upload = multer({storage })
+//   export default upload;
+
+// backend/utils/cloudinary.js
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
