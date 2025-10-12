bc2d4243d7a231b5d71b7fa5a1e8874a6c74bceb
Shivam Gupta <shivamgupta86923@gmail.com>
2025-10-12 13:09:19 +0530

    Update Home.jsx

--- my-react-app/src/components/home/Home.jsx
+++ my-react-app/src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Banner from '../banner/Banner';
 import Categories from './Categories';
-import { Grid, Box } from '@mui/material'; 
+import { Grid } from '@mui/material'; 
 import Posts from './Posts';
 
 const Home = () => {
@@ -10,31 +10,24 @@ const Home = () => {
       <Banner />
       
       <Grid container sx={{ width: '100%' }}>
-        {/* Categories - Right side on desktop, Full width on mobile */}
+        {/* Categories - Left side on desktop, Full width on mobile */}
         <Grid 
           item 
           lg={2} 
-          md={3}
           sm={12}
           xs={12}
-          sx={{
-            order: { xs: 1, sm: 1, md: 1, lg: 2 }
-          }}
         >
           <Categories />
         </Grid>
 
-        {/* Posts - Left side on desktop, Full width below on mobile */}
+        {/* Posts - Right side on desktop, Full width on mobile (2 per row) */}
         <Grid 
           container 
           item 
           lg={10} 
-          md={9}
           sm={12}
           xs={12}
-          sx={{
-            order: { xs: 2, sm: 2, md: 2, lg: 1 }
-          }}
+          spacing={2}
         >
           <Posts />
         </Grid>
