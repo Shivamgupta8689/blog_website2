13da43241cfc260e9821820ec48a44cd0af34308
Shivam Gupta <shivamgupta86923@gmail.com>
2025-10-12 13:04:21 +0530

    Update Home.jsx

--- my-react-app/src/components/home/Home.jsx
+++ my-react-app/src/components/home/Home.jsx
@@ -1,46 +1,44 @@
 import React from 'react'
 import Banner from '../banner/Banner';
 import Categories from './Categories';
-import { Grid, Box, useMediaQuery, useTheme } from '@mui/material'; 
+import { Grid, Box } from '@mui/material'; 
 import Posts from './Posts';
 
 const Home = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
   return (
     <>
       <Banner />
       
-      {isMobile ? (
-        // Mobile Layout - Categories Full Width, Posts below
-        <Box sx={{ width: '100%', padding: '0 0' }}>
-          <Grid item xs={12} sx={{ width: '100%' }}>
-            <Categories />
-          </Grid>
-          <Grid 
-            container 
-            spacing={1}
-            sx={{
-              width: '100%',
-              padding: '0 8px',
-              margin: 0
-            }}
-          >
-            <Posts />
-          </Grid>
-        </Box>
-      ) : (
-        // Desktop/Tablet Layout - Original 2 column
-        <Grid container sx={{ width: '100%' }}>
-          <Grid item lg={2} sm={2} xs={12}>
-            <Categories />
-          </Grid>
-          <Grid container item lg={10} sm={10} xs={12}>
-            <Posts />
-          </Grid>
+      <Grid container sx={{ width: '100%' }}>
+        {/* Categories - Right side on desktop, Full width on mobile */}
+        <Grid 
+          item 
+          lg={2} 
+          md={3}
+          sm={12}
+          xs={12}
+          sx={{
+            order: { xs: 1, sm: 1, md: 1, lg: 2 }
+          }}
+        >
+          <Categories />
         </Grid>
-      )}
+
+        {/* Posts - Left side on desktop, Full width below on mobile */}
+        <Grid 
+          container 
+          item 
+          lg={10} 
+          md={9}
+          sm={12}
+          xs={12}
+          sx={{
+            order: { xs: 2, sm: 2, md: 2, lg: 1 }
+          }}
+        >
+          <Posts />
+        </Grid>
+      </Grid>
     </>
   )
 }
